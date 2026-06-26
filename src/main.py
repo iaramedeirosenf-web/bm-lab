476ba022ffc3e5f585e2d1027d6b080c8263efe7
iaramedeirosenf-web <iaramedeirosenf@gmail.com>
2026-06-26 19:34:27 -0300

    cria primeiro  programa BM Lab

---
+++ src/main.py
@@ -0,0 +1,9 @@
+print("=" * 35)
+print("      BM LAB v0.1")
+print("=" * 35)
+
+print()
+print("1 - Classificar concurso")
+print("2 - Gerar Banco Mestre")
+print("3 - Auditoria")
+print("4 - Sair")
